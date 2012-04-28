6184a7692bf56fde54662e997d627fbd67a2f74a
Alex TD (Mac) <alextd@yahoo.com>
2012-04-28 00:01:54 -0700

    wider tutorial and player tracking

--- impact/lib/game/entities/ball-player.js
+++ impact/lib/game/entities/ball-player.js
@@ -19,6 +19,20 @@ EntityBallPlayer = EntityBall.extend({
 		this.wantSwing = ig.input.state('z') || ig.input.state('click');
 
 		this.parent();
+		ig.game.screen.x = this.pos.x - ig.system.width/2 + this.size.x/2;
+		ig.game.screen.y = this.pos.y - ig.system.height/2 + this.size.y/2;
+		if(ig.game.screen.x < 0)
+			ig.game.screen.x = 0;
+		if(ig.game.screen.y < 0)
+			ig.game.screen.y = 0;
+
+		var tileSize = ig.game.collisionMap.tilesize;
+
+		if(ig.game.screen.y + ig.system.height > ig.game.collisionMap.height * tileSize)
+			ig.game.screen.y = ig.game.collisionMap.height * tileSize - ig.system.height;
+		if(ig.game.screen.x + ig.system.width > ig.game.collisionMap.width * tileSize )
+			ig.game.screen.x = ig.game.collisionMap.width * tileSize - ig.system.width;
+
 	}
 });
 
